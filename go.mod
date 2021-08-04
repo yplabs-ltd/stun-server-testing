e6f631b4b28115a8497a5373b2ac85c6c7483b1f
SlZeroth <qkrwncks593@gmail.com>
2021-08-04 15:01:35 +0900

    바인딩 리퀘스트 처리

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/yplabs-ltd/stun-server-testing
+
+go 1.15
+
+require (
+	github.com/pion/stun v0.3.5
+)
--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/pion/stun"
+)
 
+func main() {
+	c, err := stun.Dial("udp", "stun.l.google.com:19302")
+	if err != nil {
+		panic(err)
+	}
+	
+	message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
+	// Sending request to STUN server, waiting for response message.
+	if err := c.Do(message, func(res stun.Event) {
+		if res.Error != nil {
+			panic(res.Error)
+		}
+		// Decoding XOR-MAPPED-ADDRESS attribute from message.
+		var xorAddr stun.XORMappedAddress
+		if err := xorAddr.GetFrom(res.Message); err != nil {
+			panic(err)
+		}
+		fmt.Println("your IP is", xorAddr.IP)
+	}); err != nil {
+		panic(err)
+	}
 }
